cca6f0dd4de65ea0f2176ac1a05911b945306e4d
nguyenvannguyen09081 <nguyendtr03@gmail.com>
2023-02-27 10:25:02 +0700

    fist commit

---
+++ baithuchanh2402/src/Slide35.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Slide35 {
+    public static void main(String[] arg) {
+        float a,b,min;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap a :");
+        a = sc.nextInt();
+        System.out.println("Nhap b :");
+        b = sc.nextInt();
+        min = a > b ? b : a;
+        System.out.println("gia tri nho nhat :" + min);
+    }
+}
